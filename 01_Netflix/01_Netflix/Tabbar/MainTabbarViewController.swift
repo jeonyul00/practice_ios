9c037b37ce2b077e22f98e85bc7d00e99b5968e5
jeonyul <jeonyul00@gmail.com>
2023-12-18 15:42:24 +0900

    feat: navigation bar

--- 01_Netflix/01_Netflix/Tabbar/MainTabbarViewController.swift
+++ 01_Netflix/01_Netflix/Tabbar/MainTabbarViewController.swift
@@ -13,9 +13,15 @@ class MainTabbarViewController: UITabBarController {
         super.viewDidLoad()
         
         // home view controller
-        let homeVC = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
-        homeVC.tabBarItem.title = "home"
-        homeVC.tabBarItem.image = UIImage(systemName: "house")
+//        let homeVC = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+//        homeVC.tabBarItem.title = "home"
+//        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        
+        // home navi view controller
+        let homeNaviVC = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "HomeNaviViewController") as! HomeNaviViewController
+        
+        homeNaviVC.tabBarItem.title = "home"
+        homeNaviVC.tabBarItem.image = UIImage(systemName: "house")
         
         // newhot view controller
         let newHotVC = UIStoryboard(name: "NewHot", bundle: nil).instantiateViewController(withIdentifier: "NewHotViewController") as! NewHotViewController
@@ -35,7 +41,7 @@ class MainTabbarViewController: UITabBarController {
         tabbarAppearance.compactInlineLayoutAppearance = tabbarItemAppearance
         self.tabBar.standardAppearance = tabbarAppearance
         self.tabBar.scrollEdgeAppearance = tabbarAppearance
-        self.viewControllers = [homeVC, newHotVC]
+        self.viewControllers = [homeNaviVC, newHotVC]
     }
     
 }
