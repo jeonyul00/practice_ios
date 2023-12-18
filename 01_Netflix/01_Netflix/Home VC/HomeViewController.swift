68dc002a59d44d1db73035fe22fa856558895ca0
jeonyul <jeonyul00@gmail.com>
2023-12-18 14:37:14 +0900

    first commit

---
+++ 01_Netflix/01_Netflix/Home VC/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  01_Netflix
+//
+//  Created by 전율 on 2023/12/18.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 01_Netflix/01_Netflix/Tabbar/MainTabbarViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabbarViewController.swift
+//  01_Netflix
+//
+//  Created by 전율 on 2023/12/18.
+//
+
+import UIKit
+
+class MainTabbarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // home view controller
+        let homeVC = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        homeVC.tabBarItem.title = "home"
+        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        
+        // newhot view controller
+        let newHotVC = UIStoryboard(name: "NewHot", bundle: nil).instantiateViewController(withIdentifier: "NewHotViewController") as! NewHotViewController
+        newHotVC.tabBarItem.title = "new & hot"
+        newHotVC.tabBarItem.image = UIImage(systemName: "play.rectangle.on.rectangle")
+        
+        let tabbarAppearance = UITabBarAppearance()
+//        tabbarAppearance.backgroundColor = .black
+        tabbarAppearance.backgroundEffect = UIBlurEffect(style: .dark)
+        let tabbarItemAppearance = UITabBarItemAppearance()
+        tabbarItemAppearance.normal.iconColor = .gray
+        tabbarItemAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.gray]
+        tabbarItemAppearance.selected.iconColor = .white
+        tabbarItemAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.white]
+        tabbarAppearance.inlineLayoutAppearance = tabbarItemAppearance
+        tabbarAppearance.stackedLayoutAppearance = tabbarItemAppearance
+        tabbarAppearance.compactInlineLayoutAppearance = tabbarItemAppearance
+        self.tabBar.standardAppearance = tabbarAppearance
+        self.tabBar.scrollEdgeAppearance = tabbarAppearance
+        self.viewControllers = [homeVC, newHotVC]
+    }
+    
+}
