693f073263719c63939f4d06072931e569509035
jeon yul <jeonyul.citylabs@gmail.com>
2023-12-20 18:38:29 +0900

    ing: home screen

--- 04_KakaoTV/04_KakaoTV/Login/LoginViewController.swift
+++ 04_KakaoTV/04_KakaoTV/Login/LoginViewController.swift
@@ -22,9 +22,9 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func buttonDidTap(_ sender: Any) {
+        self.view.window?.rootViewController = self.storyboard?.instantiateViewController(withIdentifier: "tabbar")
         
     }
     
     
 }
-
