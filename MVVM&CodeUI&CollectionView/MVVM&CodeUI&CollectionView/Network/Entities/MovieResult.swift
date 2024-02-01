1c472ca97585f65fdac4d33ea9ba75d2da9928a6
jeon yul <jeonyul.citylabs@gmail.com>
2024-02-01 12:02:58 +0900

    update

---
+++ MVVM&CodeUI&CollectionView/MVVM&CodeUI&CollectionView/Network/Entities/MovieResult.swift
@@ -0,0 +1,14 @@
+//
+//  MovieResults.swift
+//  MVVM&CodeUI&CollectionView
+//
+//  Created by 전율 on 2024/02/01.
+//
+
+import Foundation
+
+struct MovieResult {
+    let upcoming: MovieListModel
+    let popular: MovieListModel
+    let nowPlaying: MovieListModel
+}
---
+++ MVVM&CodeUI&CollectionView/Pods/Target Support Files/Pods-MVVM&CodeUI&CollectionView/Pods-MVVM&CodeUI&CollectionView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVVM_CodeUI_CollectionView : NSObject
+@end
+@implementation PodsDummy_Pods_MVVM_CodeUI_CollectionView
+@end
