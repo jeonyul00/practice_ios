411e3644b8070ee12e16c9c661a8b6ed6678a031
jeonyul <jeonyul00@gmail.com>
2024-06-05 17:31:29 +0900

    update

--- AutoLayout/Project/Section3/FitnessAppScroll/FitnessAppScroll/FitnessView.swift
+++ AutoLayout/Project/Section3/FitnessAppScroll/FitnessAppScroll/FitnessView.swift
@@ -9,6 +9,30 @@ import UIKit
 
 class FitnessView: UIView {
 
-    
+    @IBOutlet weak var bgImageView: UIImageView!
+    @IBOutlet weak var fitnessTitle: UILabel!
+        
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadNib()
+    }
 
+    // 초기화 시 nib 파일도 같이 호출되도록
+    init() {
+        super.init(frame: .zero)
+        loadNib()
+    }
+    
+    private func loadNib(){
+        if let loadedView = UINib(nibName: "FitnessView", bundle: nil).instantiate(withOwner: self).first as? UIView {
+            self.addSubview(loadedView)
+            loadedView.translatesAutoresizingMaskIntoConstraints = false
+            loadedView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+            loadedView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+            loadedView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+            loadedView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        }
+    }
+    
+    
 }
--- AutoLayout/Project/Section3/FitnessAppScroll/FitnessAppScroll/ViewController.swift
+++ AutoLayout/Project/Section3/FitnessAppScroll/FitnessAppScroll/ViewController.swift
@@ -23,14 +23,39 @@ class ViewController: UIViewController {
         return stackView
     }()
     
+    let fitnessViewList:[FitnessView] = {
+        var fvList = [FitnessView]()
+        (0..<10).forEach { num in
+            let fv = FitnessView()
+            fvList.append(fv)
+        }
+        return fvList
+    }()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
+        fitnessViewList.forEach { view in
+            view.heightAnchor.constraint(equalToConstant: 150).isActive = true
+            stackView.addArrangedSubview(view)
+        }
+        
+        scrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        
+        // !!!!!!!!!!!!!!!!!!!!!!
+        scrollView.contentLayoutGuide.leadingAnchor.constraint(equalTo: stackView.leadingAnchor).isActive = true
+        scrollView.contentLayoutGuide.bottomAnchor.constraint(equalTo: stackView.bottomAnchor).isActive = true
+        scrollView.contentLayoutGuide.topAnchor.constraint(equalTo: stackView.topAnchor).isActive = true
+        scrollView.contentLayoutGuide.trailingAnchor.constraint(equalTo: stackView.trailingAnchor).isActive = true
+        scrollView.frameLayoutGuide.leadingAnchor.constraint(equalTo: stackView.leadingAnchor).isActive = true
+        scrollView.frameLayoutGuide.trailingAnchor.constraint(equalTo: stackView.trailingAnchor).isActive = true
+        
         
     }
-
 
 }
 
