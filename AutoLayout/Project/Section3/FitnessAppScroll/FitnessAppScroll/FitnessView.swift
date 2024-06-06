94b4d9bb56e0341d684ff60ff085484c9c972e13
jeonyul <jeonyul00@gmail.com>
2024-06-06 11:58:50 +0900

    update

--- AutoLayout/Project/Section3/FitnessAppScroll/FitnessAppScroll/FitnessView.swift
+++ AutoLayout/Project/Section3/FitnessAppScroll/FitnessAppScroll/FitnessView.swift
@@ -8,20 +8,26 @@
 import UIKit
 
 class FitnessView: UIView {
-
-    @IBOutlet weak var bgImageView: UIImageView!
+    
+    @IBOutlet weak var bgImageView: UIImageView! {
+        didSet {
+            bgImageView.contentMode = .scaleAspectFill
+        }
+    }
     @IBOutlet weak var fitnessTitle: UILabel!
-        
+    
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         loadNib()
     }
-
+    
     // 초기화 시 nib 파일도 같이 호출되도록
     init() {
         super.init(frame: .zero)
         loadNib()
     }
+        
     
     private func loadNib(){
         if let loadedView = UINib(nibName: "FitnessView", bundle: nil).instantiate(withOwner: self).first as? UIView {
--- AutoLayout/Project/Section3/FitnessAppScroll/FitnessAppScroll/ViewController.swift
+++ AutoLayout/Project/Section3/FitnessAppScroll/FitnessAppScroll/ViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var planList = [PlanList]() {
+        didSet {
+            if fitnessViewList.isEmpty {
+                var fvList = [FitnessView]()
+                (0..<planList.count).forEach { num in
+                    let fv = FitnessView()
+                    fvList.append(fv)
+                }
+                fitnessViewList  = fvList
+            }
+                        
+            stackView.arrangedSubviews.forEach { view in
+                stackView.removeArrangedSubview(view)
+                view.removeFromSuperview()
+            }
+            
+            fitnessViewList.forEach { view in
+                view.heightAnchor.constraint(equalToConstant: 150).isActive = true
+                stackView.addArrangedSubview(view)
+            }
+            
+            for (idx,view) in fitnessViewList.enumerated() {
+                view.bgImageView.image = UIImage(named: planList[idx].imageName)
+                view.fitnessTitle.text = planList[idx].planType
+            }
+        }
+    }
+    
     let scrollView:UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.backgroundColor = .yellow
         return scrollView
     }()
     
@@ -23,30 +50,19 @@ class ViewController: UIViewController {
         return stackView
     }()
     
-    let fitnessViewList:[FitnessView] = {
-        var fvList = [FitnessView]()
-        (0..<10).forEach { num in
-            let fv = FitnessView()
-            fvList.append(fv)
-        }
-        return fvList
-    }()
+    var fitnessViewList = [FitnessView]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addSubview(scrollView)
         scrollView.addSubview(stackView)
-        fitnessViewList.forEach { view in
-            view.heightAnchor.constraint(equalToConstant: 150).isActive = true
-            stackView.addArrangedSubview(view)
-        }
         
         scrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
         scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
         
-        // !!!!!!!!!!!!!!!!!!!!!!
+        // !!!!!!!!!!!!!!!!!!!!!! 스크롤뷰 콘텐츠 레이아우스 프레임 레이아웃
         scrollView.contentLayoutGuide.leadingAnchor.constraint(equalTo: stackView.leadingAnchor).isActive = true
         scrollView.contentLayoutGuide.bottomAnchor.constraint(equalTo: stackView.bottomAnchor).isActive = true
         scrollView.contentLayoutGuide.topAnchor.constraint(equalTo: stackView.topAnchor).isActive = true
@@ -54,8 +70,18 @@ class ViewController: UIViewController {
         scrollView.frameLayoutGuide.leadingAnchor.constraint(equalTo: stackView.leadingAnchor).isActive = true
         scrollView.frameLayoutGuide.trailingAnchor.constraint(equalTo: stackView.trailingAnchor).isActive = true
         
+        loadJsonData()
         
     }
-
+    
+    func loadJsonData(){
+        guard let plans = plans else { return }
+        do {
+            planList = try JSONDecoder().decode([PlanList].self, from: plans)
+        } catch {
+            print(error)
+        }
+    }
+    
 }
 
