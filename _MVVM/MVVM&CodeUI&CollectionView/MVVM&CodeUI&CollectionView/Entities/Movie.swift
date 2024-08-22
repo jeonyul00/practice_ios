651a214ff15172754351504f9f955081b7913d9a
jeonyul <jeonyul00@gmail.com>
2024-08-22 17:40:31 +0900

    update

---
+++ _MVVM/MVVM&CodeUI&CollectionView/MVVM&CodeUI&CollectionView/Entities/Movie.swift
@@ -0,0 +1,39 @@
+//
+//  Movie.swift
+//  MVVM&CodeUI&CollectionView
+//
+//  Created by 전율 on 8/22/24.
+//
+
+import Foundation
+
+struct Movie:Decodable {
+    
+    let title: String
+    let overview: String
+    let posterURL: String
+    let vote: String
+    let releaseDate:String
+    
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case overview
+        case posterPath = "poster_path"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+        case releaseDate = "release_Date"
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        title = try container.decode(String.self, forKey: .title)
+        overview = try container.decode(String.self, forKey: .overview)
+        let path = try container.decode(String.self, forKey: .posterPath)
+        posterURL = "https://image.tmdb.org/t/p/w500\(path)"
+        let voteAverage = try container.decode(String.self, forKey: .voteAverage)
+        let voteCount = try container.decode(String.self, forKey: .voteCount)
+        vote = "\(voteAverage) (\(voteCount))"
+        releaseDate = try container.decode(String.self, forKey: .releaseDate)
+    }
+
+}
