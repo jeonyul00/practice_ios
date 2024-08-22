ed0ec4a8550c34deeaea40f46175c50c60700701
jeonyul <jeonyul00@gmail.com>
2024-08-22 16:30:26 +0900

    update

--- _MVVM/MVVM&CodeUI&CollectionView/MVVM&CodeUI&CollectionView/Network/Const.swift
+++ _MVVM/MVVM&CodeUI&CollectionView/MVVM&CodeUI&CollectionView/Network/Const.swift
@@ -2,10 +2,10 @@
 //  Const.swift
 //  MVVM&CodeUI&CollectionView
 //
-//  Created by 전율 on 2024/02/01.
+//  Created by 전율 on 8/22/24.
 //
 
 import Foundation
 
-let APIKEY = "b6386880decffc8ccc0938bd58f71607"
-let API_READ_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNjM4Njg4MGRlY2ZmYzhjY2MwOTM4YmQ1OGY3MTYwNyIsInN1YiI6IjY1YmFmNjM2YzE0NGRkMDE3Y2ZkZDM2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oCF6625UdqjjYV0C4BNKEkwNn5erYLN5Hk7m68NXYhc"
+let APIKEY="92be1a55a0822ae8cf51184805441bed"
+
--- _MVVM/MVVM&CodeUI&CollectionView/MVVM&CodeUI&CollectionView/ViewController.swift
+++ _MVVM/MVVM&CodeUI&CollectionView/MVVM&CodeUI&CollectionView/ViewController.swift
@@ -13,53 +13,10 @@ import RxCocoa
 
 class ViewController: UIViewController {
     
-    let buttonView = ButtonView()
-    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
-    let viewModel = ViewModel()
-    let tvTrigger = PublishSubject<Void>()
-    let movieTrigger = PublishSubject<Void>()
-    let disposeBag = DisposeBag()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUI()
-        bindViewModel()
-        bindView()
-    }
-    
-    
-    private func setUI() -> Void {
-        self.view.addSubview(buttonView)
-        self.view.addSubview(collectionView)
-        
-        buttonView.snp.makeConstraints { make in
-            make.top.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
-            make.height.equalTo(80)
-        }
-        
-        collectionView.snp.makeConstraints { make in
-            make.leading.trailing.bottom.equalToSuperview()
-            make.top.equalTo(buttonView.snp.bottom)
-        }
         
     }
-    
-    private func bindViewModel() {
-        let input = ViewModel.Input(tvTrigger: tvTrigger.asObservable(), movieTrigger: movieTrigger.asObservable())
-        let output = viewModel.transform(input: input)
-        output.tvList.bind { tvList in  print(tvList) }.disposed(by: disposeBag)
-    }
-    
-    private func bindView() {
-        buttonView.tvButton.rx.tap.bind { [weak self] in
-            self?.tvTrigger.onNext(Void())
-        }.disposed(by: disposeBag)
-        
-        buttonView.movieButton.rx.tap.bind {[weak self] in
-            self?.movieTrigger.onNext(Void())
-        }.disposed(by: disposeBag)
-    }
-    
     
     
 }
