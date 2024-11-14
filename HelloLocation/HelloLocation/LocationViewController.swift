9b61ed3553f034cb0c7fcd79088f45b26e355675
jeonyul <jeonyul00@gmail.com>
2024-11-14 12:44:28 +0900

    update

--- HelloLocation/HelloLocation/LocationViewController.swift
+++ HelloLocation/HelloLocation/LocationViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import CoreLocation
 
 class LocationViewController: UIViewController {
-
+    
     @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var adressLabel: UILabel!
@@ -18,19 +18,63 @@ class LocationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        manager.delegate = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        if CLLocationManager.locationServicesEnabled() {
-            
-        } else {
-            let alert = UIAlertController(title: "알림", message: "위치 서비스를 활성화시켜 주세요.", preferredStyle: .alert)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if manager.authorizationStatus == .authorizedWhenInUse || manager.authorizationStatus == .authorizedAlways {
+            manager.startUpdatingLocation()
         }
     }
-
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        manager.stopUpdatingLocation()
+    }
 }
 
+extension LocationViewController:CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .notDetermined: // 미선택
+            manager.requestAlwaysAuthorization()
+            break
+        case .restricted, .denied:
+            let alert = UIAlertController(title: "알림", message: "위치 서비스를 활성화시켜 주세요.", preferredStyle: .alert)
+            let settingAction = UIAlertAction(title: "설정", style: .default) { _ in
+                if let url = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(url)
+                }
+            }
+            let cacelAction = UIAlertAction(title: "취소", style: .cancel)
+            alert.addAction(settingAction)
+            alert.addAction(cacelAction)
+            self.present(alert, animated: true)
+            break
+        case .authorizedAlways, .authorizedWhenInUse: // 사용중일떄만 허용: // 항상 허용
+            manager.startUpdatingLocation()
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        let error = error as NSError
+        guard error.code != CLError.Code.locationUnknown.rawValue else { return }
+        print(error)
+        manager.stopUpdatingLocation()
+    }
+    
+    // 위치가 바뀔때마다 호출
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let currnetLocation = locations.last {
+            latitudeLabel.text = "\(currnetLocation.coordinate.latitude)"
+            longitudeLabel.text = "\(currnetLocation.coordinate.longitude)"
+        }
+    }
+}
--- HelloLocation/HelloLocation/MapViewController.swift
+++ HelloLocation/HelloLocation/MapViewController.swift
@@ -6,24 +6,76 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        manager.delegate = self
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if manager.authorizationStatus == .authorizedWhenInUse || manager.authorizationStatus == .authorizedAlways {
+            manager.startUpdatingLocation()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        manager.stopUpdatingLocation()
     }
-    */
 
+    
+    func move(to location: CLLocation) {
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 100, longitudinalMeters: 100)
+        mapView.setRegion(region, animated: true)
+    }
+    
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .notDetermined: // 미선택
+            manager.requestAlwaysAuthorization()
+            break
+        case .restricted, .denied:
+            let alert = UIAlertController(title: "알림", message: "위치 서비스를 활성화시켜 주세요.", preferredStyle: .alert)
+            let settingAction = UIAlertAction(title: "설정", style: .default) { _ in
+                if let url = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(url)
+                }
+            }
+            let cacelAction = UIAlertAction(title: "취소", style: .cancel)
+            alert.addAction(settingAction)
+            alert.addAction(cacelAction)
+            self.present(alert, animated: true)
+            break
+        case .authorizedAlways, .authorizedWhenInUse: // 사용중일떄만 허용: // 항상 허용
+            manager.startUpdatingLocation()
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        let error = error as NSError
+        guard error.code != CLError.Code.locationUnknown.rawValue else { return }
+        print(error)
+        manager.stopUpdatingLocation()
+    }
+    
+    // 위치가 바뀔때마다 호출
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let currnetLocation = locations.last {
+            move(to: currnetLocation)
+        }
+    }
+
+    
 }
