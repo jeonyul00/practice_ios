fdba817767650c2c7ee6f2e15ac378c2032c7601
jeonyul <jeonyul00@gmail.com>
2024-11-28 22:00:53 +0900

    feat: LimitedLibraryViewController

--- HelloPhotos/HelloPhotos/LimitedLibraryViewController.swift
+++ HelloPhotos/HelloPhotos/LimitedLibraryViewController.swift
@@ -22,10 +22,49 @@ class LimitedLibraryViewController: UIViewController {
         let option = PHFetchOptions()
         option.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
         allPhotos = PHAsset.fetchAssets(with: option)
+        
+        // changedOberver
+        PHPhotoLibrary.shared().register(self)
+    }
+    
+    deinit {
+        PHPhotoLibrary.shared().unregisterChangeObserver(self)
     }
     
     @IBAction func editSelectedAssets(_ sender: Any) {
         PHPhotoLibrary.shared().presentLimitedLibraryPicker(from: self)
+    }
+}
+
+extension LimitedLibraryViewController: PHPhotoLibraryChangeObserver {
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        DispatchQueue.main.async { [weak self] in
+            guard let self, let result = self.allPhotos else { return }
+            guard let changes = changeInstance.changeDetails(for: result) else { return }
+            self.allPhotos = changes.fetchResultAfterChanges
+            
+            if changes.hasIncrementalChanges {
+                // 배치 업데이트
+                self.collectionView.performBatchUpdates {
+                    if let removeIndexPaths = changes.removedIndexes?.map({IndexPath(item: $0, section: 0)}),!removeIndexPaths.isEmpty  {
+                        self.collectionView.deleteItems(at: removeIndexPaths)
+                    }
+                    
+                    if let insertedIndexPaths = changes.insertedIndexes?.map({IndexPath(item: $0, section:0)}), !insertedIndexPaths.isEmpty {
+                        self.collectionView.insertItems(at: insertedIndexPaths)
+                    }
+                    changes.enumerateMoves { from, to in
+                        self.collectionView.moveItem(at: IndexPath(item: from, section: 0), to: IndexPath(item: to, section: 0))
+                    }
+                }
+                
+                if let changedIndexPaths = changes.changedIndexes?.map({ IndexPath(item: $0, section: 0)}), !changedIndexPaths.isEmpty {
+                    self.collectionView.reloadItems(at: changedIndexPaths)
+                }
+            } else {
+                self.collectionView.reloadData()
+            }
+        }
     }
 }
 
@@ -45,7 +84,7 @@ extension LimitedLibraryViewController:UICollectionViewDataSource {
             imageManager.requestImage(for: target, targetSize: size, contentMode: .aspectFill, options:nil) { image, _ in
                 cell.imageView.image = image
             }
-        }        
+        }
         return cell
     }
     
