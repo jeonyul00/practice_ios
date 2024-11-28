a42fe69fb6881a7d476cd7728ecfab75eb305fd0
jeonyul <jeonyul00@gmail.com>
2024-11-28 14:09:02 +0900

    feat: pick photo

---
+++ HelloPhotos/HelloPhotos/PickerViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PickerViewController.swift
+//  HelloPhotos
+//
+//  Created by 전율 on 11/28/24.
+//
+
+import UIKit
+import PhotosUI
+
+class PickerViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func pick(_ sender: Any) {
+        var configuration = PHPickerConfiguration(photoLibrary: .shared())
+        configuration.filter = .images
+        configuration.preferredAssetRepresentationMode = .current
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        self.present(picker, animated: true)
+    }
+    
+}
+
+extension PickerViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {        
+        if let first = results.first {
+            if first.itemProvider.canLoadObject(ofClass: UIImage.self) {
+                first.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                    DispatchQueue.main.async {
+                        self?.imageView.image = image as? UIImage
+                    }
+                }
+            }
+        }
+        
+        picker.dismiss(animated: true)
+    }
+}
